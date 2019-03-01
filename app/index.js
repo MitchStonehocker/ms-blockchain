b8d56ac615124019e5b39d7961664872a3ddc258
Mitch Stonehocker <mtichstonehocker@gmail.com>
2019-02-28 19:01:56 -0500

    json handling add, get and post api tested

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 // app/index.js
 
 const express = require('express')
+const bodyParser = require('body-parser')
 const Blockchain = require('../blockchain')
 
 const HTTP_PORT = process.env.HTTP_PORT || 3100
@@ -9,8 +10,17 @@ const HTTP_PORT = process.env.HTTP_PORT || 3100
 const app = express()
 const bc = new Blockchain()
 
+app.use(bodyParser.json())
+
 app.get('/blocks', (req, res) => {
   res.json(bc.chain)
+})
+
+app.post('/mine', (req, res) => {
+  const block = bc.addBlock(req.body.data)
+  console.log(`New block added: ${block.toString()}`)
+
+  res.redirect('/blocks')
 })
 
 app.listen(HTTP_PORT, () => console.log(`Listing on port ${HTTP_PORT}`))
